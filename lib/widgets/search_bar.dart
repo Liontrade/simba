3889a993753b22f17b78d41fe239b4a1c965bb62
Arkadiusz4 <arekmika4@gmail.com>
2024-09-06 22:22:42 +0200

    Update search_bar.dart

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:simba/style/_style.dart';
 
-class SearchBar extends StatelessWidget {
-  const SearchBar({super.key});
+class LtSearchBar extends StatelessWidget {
+  const LtSearchBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: TextField(
-        decoration: InputDecoration(
-          prefixIcon: Icon(Icons.search),
-          hintText: 'Search for stocks',
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide.none,
+      child: Container(
+        decoration: BoxDecoration(
+          color: LtColor.white,
+          borderRadius: BorderRadius.circular(16.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 15,
+              blurRadius: 30,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: TextField(
+          keyboardType: TextInputType.text,
+          style: LtTextStyle.manrope16medium,
+          decoration: InputDecoration(
+            prefixIcon: const Icon(Icons.search),
+            hintText: 'Search for stocks',
+            hintStyle: LtTextStyle.manrope14regular,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: BorderSide.none,
+            ),
+            filled: true,
+            fillColor: LtColor.white,
           ),
-          filled: true,
-          fillColor: Colors.grey[200],
         ),
       ),
     );
