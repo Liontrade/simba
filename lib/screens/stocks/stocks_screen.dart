b2647028b35dbd7dfc992b1bca60c2c7a9a9d0d7
Arkadiusz4 <arekmika4@gmail.com>
2024-09-25 21:37:09 +0200

    Update stocks_screen.dart

--- lib/screens/stocks/stocks_screen.dart
+++ lib/screens/stocks/stocks_screen.dart
@@ -10,221 +10,6 @@ class StocksPage extends StatefulWidget {
   @override
   _StocksPageState createState() => _StocksPageState();
 }
-//
-// class _StocksPageState extends State<StocksPage> {
-//   int _selectedTabIndex = 0;
-//
-//   final List<Map<String, dynamic>> _stocks = [
-//     {
-//       "symbol": "AAPL",
-//       "company": "Apple Inc",
-//       "price": 186.88,
-//       "change": 10.57,
-//       "icon": Icons.apple,
-//       "liked": false,
-//     },
-//     {
-//       "symbol": "TSLA",
-//       "company": "Tesla Inc",
-//       "price": 173.74,
-//       "change": 7.16,
-//       "icon": Icons.electric_car,
-//       "liked": true,
-//     },
-//     {
-//       "symbol": "META",
-//       "company": "Meta Inc",
-//       "price": 465.78,
-//       "change": -5.62,
-//       "icon": Icons.mediation,
-//       "liked": false,
-//     },
-//     {
-//       "symbol": "AMZN",
-//       "company": "Amazon Inc",
-//       "price": 181.05,
-//       "change": 2.53,
-//       "icon": Icons.shopping_cart,
-//       "liked": true,
-//     },
-//     {
-//       "symbol": "MSFT",
-//       "company": "Microsoft Corp",
-//       "price": 427.00,
-//       "change": 4.39,
-//       "icon": Icons.computer,
-//       "liked": false,
-//     },
-//     {
-//       "symbol": "AAPL",
-//       "company": "Apple Inc",
-//       "price": 186.88,
-//       "change": 10.57,
-//       "icon": Icons.apple,
-//       "liked": false,
-//     },
-//     {
-//       "symbol": "TSLA",
-//       "company": "Tesla Inc",
-//       "price": 173.74,
-//       "change": 7.16,
-//       "icon": Icons.electric_car,
-//       "liked": false,
-//     },
-//     {
-//       "symbol": "META",
-//       "company": "Meta Inc",
-//       "price": 465.78,
-//       "change": -5.62,
-//       "icon": Icons.mediation,
-//       "liked": false,
-//     },
-//     {
-//       "symbol": "AMZN",
-//       "company": "Amazon Inc",
-//       "price": 181.05,
-//       "change": 2.53,
-//       "icon": Icons.shopping_cart,
-//       "liked": false,
-//     },
-//     {
-//       "symbol": "MSFT",
-//       "company": "Microsoft Corp",
-//       "price": 427.00,
-//       "change": 4.39,
-//       "icon": Icons.computer,
-//       "liked": false,
-//     },
-//     {
-//       "symbol": "AAPL",
-//       "company": "Apple Inc",
-//       "price": 186.88,
-//       "change": 10.57,
-//       "icon": Icons.apple,
-//       "liked": false,
-//     },
-//     {
-//       "symbol": "TSLA",
-//       "company": "Tesla Inc",
-//       "price": 173.74,
-//       "change": 7.16,
-//       "icon": Icons.electric_car,
-//       "liked": false,
-//     },
-//     {
-//       "symbol": "META",
-//       "company": "Meta Inc",
-//       "price": 465.78,
-//       "change": -5.62,
-//       "icon": Icons.mediation,
-//       "liked": false,
-//     },
-//     {
-//       "symbol": "AMZN",
-//       "company": "Amazon Inc",
-//       "price": 181.05,
-//       "change": 2.53,
-//       "icon": Icons.shopping_cart,
-//       "liked": false,
-//     },
-//     {
-//       "symbol": "MSFT",
-//       "company": "Microsoft Corp",
-//       "price": 427.00,
-//       "change": 4.39,
-//       "icon": Icons.computer,
-//       "liked": false,
-//     },
-//   ];
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final List<Map<String, dynamic>> displayedStocks = _stocks.where((stock) {
-//       if (_selectedTabIndex == 0) {
-//         return true;
-//       } else if (_selectedTabIndex == 1) {
-//         return stock['liked'];
-//       }
-//       return false;
-//     }).toList();
-//
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-//       child: Column(
-//         children: [
-//           const SizedBox(height: 30),
-//           const LtSearchBar(),
-//           const SizedBox(height: 20),
-//           CustomTabBar(
-//             onTabSelected: (index) {
-//               setState(() {
-//                 _selectedTabIndex = index;
-//               });
-//             },
-//           ),
-//           Expanded(
-//             child: ListView.builder(
-//               itemCount: displayedStocks.length,
-//               itemBuilder: (context, index) {
-//                 final stock = displayedStocks[index];
-//                 final isLast = index == displayedStocks.length - 1;
-//
-//                 return GestureDetector(
-//                   onTap: () {
-//                     Navigator.push(
-//                       context,
-//                       MaterialPageRoute(
-//                         builder: (context) => StockDetailPage(
-//                           symbol: stock['symbol'],
-//                           company: stock['company'],
-//                           price: stock['price'],
-//                           change: stock['change'],
-//                         ),
-//                       ),
-//                     );
-//                   },
-//                   child: Padding(
-//                     padding: EdgeInsets.only(
-//                       bottom: isLast ? 16.0 : 0.0,
-//                     ),
-//                     child: Column(
-//                       children: [
-//                         StockTile(
-//                           companyName: stock['symbol'],
-//                           companyFullName: stock['company'],
-//                           price: stock['price'],
-//                           change: stock['change'],
-//                           iconData: stock['icon'],
-//                           isLiked: stock['liked'],
-//                           isFirst: index == 0,
-//                           isLast: isLast,
-//                           isLikedTabSelected: _selectedTabIndex == 1,
-//                           onLikePressed: () {
-//                             setState(() {
-//                               // Find the index in the _stocks list and toggle liked
-//                               final stockIndex = _stocks.indexOf(stock);
-//                               _stocks[stockIndex]['liked'] = !_stocks[stockIndex]['liked'];
-//                             });
-//                           },
-//                         ),
-//                         if (!isLast)
-//                           const Divider(
-//                             height: 1,
-//                             thickness: 1,
-//                             color: Colors.black,
-//                           ),
-//                       ],
-//                     ),
-//                   ),
-//                 );
-//               },
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
 
 class _StocksPageState extends State<StocksPage> {
   int _selectedTabIndex = 0;
@@ -243,27 +28,40 @@ class _StocksPageState extends State<StocksPage> {
     try {
       final response = await http.get(url);
       if (response.statusCode == 200) {
+        print('Response body: ${response.body}');
         final Map<String, dynamic> data = jsonDecode(response.body);
 
-        // Pobierz wszystkie klucze (daty) i sortuj je
-        final dates = data.keys.toList()..sort();
+        List<Map<String, dynamic>> stocks = [];
 
-        // Pobierz wartości zamknięcia
-        final List<double> closePrices = dates
-            .map((date) => data[date]['Close'] as double)
-            .toList();
+        data.forEach((symbol, history) {
+          // Pobierz wszystkie daty i sortuj je
+          final dates = history.keys.toList()..sort();
 
-        setState(() {
-          _stocks = [
-            {
-              "symbol": "AAPL",
-              "company": "Apple Inc",
+          // Pobierz ceny zamknięcia (Close) i upewnij się, że są one typu double
+          final List<double> closePrices = dates.map((date) {
+            final price = history[date]['Close'];
+            // Upewnij się, że cena jest typu double
+            if (price is num) {
+              return price.toDouble();
+            }
+            return 0.0; // Wartość domyślna, jeśli cena jest innego typu
+          }).toList();
+
+          // Sprawdź, czy closePrices zawiera dane
+          if (closePrices.isNotEmpty) {
+            stocks.add({
+              "symbol": symbol,
+              "company": symbol, // Możesz tu dodać pełną nazwę firmy
               "price": closePrices.last, // Ostatnia cena zamknięcia
               "change": closePrices.last - closePrices.first, // Zmiana ceny
-              "icon": Icons.apple,
+              "icon": Icons.business, // Możesz dodać odpowiednią ikonę dla każdej firmy
               "liked": false,
-            },
-          ];
+            });
+          }
+        });
+
+        setState(() {
+          _stocks = stocks;
         });
       } else {
         throw Exception('Failed to load stocks');
@@ -290,62 +88,63 @@ class _StocksPageState extends State<StocksPage> {
             },
           ),
           Expanded(
-            child: ListView.builder(
-              itemCount: _stocks.length,
-              itemBuilder: (context, index) {
-                final stock = _stocks[index];
-                final isLast = index == _stocks.length - 1;
+            child: _stocks.isEmpty
+                ? Center(child: Text('No stocks available'))
+                : ListView.builder(
+                    itemCount: _stocks.length,
+                    itemBuilder: (context, index) {
+                      final stock = _stocks[index];
+                      final isLast = index == _stocks.length - 1;
 
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => StockDetailPage(
-                          symbol: stock['symbol'],
-                          company: stock['company'],
-                          price: stock['price'],
-                          change: stock['change'],
-                        ),
-                      ),
-                    );
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                      bottom: isLast ? 16.0 : 0.0,
-                    ),
-                    child: Column(
-                      children: [
-                        StockTile(
-                          companyName: stock['symbol'],
-                          companyFullName: stock['company'],
-                          price: stock['price'],
-                          change: stock['change'],
-                          iconData: stock['icon'],
-                          isLiked: stock['liked'],
-                          isFirst: index == 0,
-                          isLast: isLast,
-                          isLikedTabSelected: _selectedTabIndex == 1,
-                          onLikePressed: () {
-                            setState(() {
-                              // Find the index in the _stocks list and toggle liked
-                              final stockIndex = _stocks.indexOf(stock);
-                              _stocks[stockIndex]['liked'] = !_stocks[stockIndex]['liked'];
-                            });
-                          },
-                        ),
-                        if (!isLast)
-                          const Divider(
-                            height: 1,
-                            thickness: 1,
-                            color: Colors.black,
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => StockDetailPage(
+                                symbol: stock['symbol'],
+                                company: stock['company'],
+                                price: stock['price'],
+                                change: stock['change'],
+                              ),
+                            ),
+                          );
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            bottom: isLast ? 16.0 : 0.0,
                           ),
-                      ],
-                    ),
+                          child: Column(
+                            children: [
+                              StockTile(
+                                companyName: stock['symbol'],
+                                companyFullName: stock['company'],
+                                price: stock['price'],
+                                change: stock['change'],
+                                iconData: stock['icon'],
+                                isLiked: stock['liked'],
+                                isFirst: index == 0,
+                                isLast: isLast,
+                                isLikedTabSelected: _selectedTabIndex == 1,
+                                onLikePressed: () {
+                                  setState(() {
+                                    final stockIndex = _stocks.indexOf(stock);
+                                    _stocks[stockIndex]['liked'] = !_stocks[stockIndex]['liked'];
+                                  });
+                                },
+                              ),
+                              if (!isLast)
+                                const Divider(
+                                  height: 1,
+                                  thickness: 1,
+                                  color: Colors.black,
+                                ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
                   ),
-                );
-              },
-            ),
           ),
         ],
       ),
